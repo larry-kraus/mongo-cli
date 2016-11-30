77dea41e6fde969f0a0334206f3d650653ccedf0
larry-kraus <krauslarry@gmail.com>
2016-11-30 08:52:47 -0700

    adding for each

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ var url = "mongodb://localhost:27017/restaurant_db";
 
  mongo.connect(url, function(err, db) {
  	var collection = db.collection('restaurants');
- 	var allChoice = prompt("Type 'all' and press enter to display all restaurants' names: ");
-	if (allChoice == "all"){
-		collection.find().toArray(function(err, docs) {
+ 	//var allChoice = prompt("Type 'all' and press enter to display all restaurants' names: ");
+	// if (allChoice == "all"){
+	// 	collection.find().toArray(function(err, docs) {
+ 	// 		console.log(docs);
+ 	// });
+ 	var restaurantChoice = prompt("Type a restaurant name and press enter to display the restaurant: ");
+	
+	if (restaurantChoice == "Cookies Corner"){
+		collection.find({"name": "Cookies Corner"}).toArray(function(err, docs) {
  			console.log(docs);
  	});
 	} else {
